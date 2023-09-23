cb4d8ab5ed7b1c76d65622140e225113f8a07696
Liver Nissan <storegame352@gmail.com>
2023-09-23 13:50:14 +0200

    first

---
+++ src/main/java/com/example/testdriven1/TestDriven1Application.java
@@ -0,0 +1,13 @@
+package com.example.testdriven1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDriven1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDriven1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdriven1/UserTest.java
@@ -0,0 +1,32 @@
+package com.example.testdriven1;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    @Test
+    public void testSetAndGetUsername() {
+        User user = new User();
+        user.setUsername("testuser");
+        assertEquals("testuser", user.getUsername());
+    }
+
+    @Test
+    public void testSetAndGetPassword() {
+        User user = new User();
+        user.setPassword("password123");
+        assertEquals("password123", user.getPassword());
+    }
+
+    @Test
+    public void testEqualsMethod() {
+        User user1 = new User("testuser", "password123");
+        User user2 = new User("testuser", "password123");
+        User user3 = new User("anotheruser", "password456");
+
+        assertTrue(user1.equals(user2));
+        assertFalse(user1.equals(user3));
+    }
+}
