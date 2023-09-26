3aff224794a659b773a944b0d49519451c0e761f
Liver Nissan <storegame352@gmail.com>
2023-09-26 11:58:28 +0200

    testar

---
+++ src/test/java/com/example/testdriven1/AuthenticationServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.testdriven1;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthenticationServiceTest {
+    @Test
+    public void testLoginWithValidCredentials() {
+        // Mocka en databas
+        Database mockDatabase = mock(Database.class);
+        when(mockDatabase.getUserByUsername("testuser")).thenReturn(new User("testuser", "password123"));
+
+        AuthenticationService authService = new AuthenticationService(mockDatabase);
+        assertTrue(authService.login("testuser", "password123"));
+    }
+
+    @Test
+    public void testLoginWithInvalidUsername() {
+        // Mocka en databas
+        Database mockDatabase = mock(Database.class);
+        when(mockDatabase.getUserByUsername("nonexistentuser")).thenReturn(null);
+
+        AuthenticationService authService = new AuthenticationService(mockDatabase);
+        assertThrows(UserNotFoundException.class, () -> authService.login("nonexistentuser", "password123"));
+    }
+
+    @Test
+    public void testLoginWithInvalidPassword() {
+        // Mocka en databas
+        Database mockDatabase = mock(Database.class);
+        when(mockDatabase.getUserByUsername("testuser")).thenReturn(new User("testuser", "password123"));
+
+        AuthenticationService authService = new AuthenticationService(mockDatabase);
+        assertThrows(InvalidPasswordException.class, () -> authService.login("testuser", "wrongpassword"));
+    }
+}
